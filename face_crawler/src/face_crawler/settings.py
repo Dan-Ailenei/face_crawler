ea3c8df471cb2d1a6d89438807aead6cb3a206a8
Dan <ailenei_d1996@yahoo.com>
2019-06-01 20:45:51 +0300

    restructered, and fixes, preparing for separation

---
+++ face_crawler/src/face_crawler/settings.py
@@ -0,0 +1,35 @@
+# 'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.100 Safari/537.36)',
+import os
+
+COOKIES_ENABLED = True
+CONCURRENT_REQUESTS = 1
+DOWNLOAD_DELAY = 5  # 0.25 - 250 ms of delay
+ROBOTSTXT_OBEY = False
+COOKIES_DEBUG = True
+SCHEDULER_DEBUG = True
+# 'LOG_FILE': 'logs.txt',
+LOG_DATEFORMAT = '%d-%m-%Y %H:%M:%S'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+JOBDIR = os.path.join(BASE_DIR, "crawls/person-1")
+
+DEFAULT_REQUEST_HEADERS = {
+    'Accept': 'text/html',
+    'Accept-Language': 'ro',
+}
+
+BOT_NAME = 'tryscrappy'
+
+SPIDER_MODULES = ['face_crawler.spiders']
+NEWSPIDER_MODULE = 'face_crawler.spiders'
+
+ROBOTSTXT_OBEY = False
+
+# custom settings
+FACEBOOK_NAME = 'Dan Ailenei'
+FACEBOOK_ID = '/dan.ailenei.9'
+NOT_FACES_LIST = \
+    [
+    'https://scontent.fclj2-1.fna.fbcdn.net/v/t1.0-1/cp0/e15/q65/c15.0.50.50a/p50x50/10354686_10150004552801856_220367501106153455_n.jpg?_nc_cat=1&efg=eyJpIjoiYiJ9&_nc_ht=scontent.fclj2-1.fna&oh=a2814e99ff91bcbfb23ee5a57b82909f&oe=5D91883F',
+    'https://scontent.fclj2-1.fna.fbcdn.net/v/t1.0-1/cp0/e15/q65/c15.0.50.50a/p50x50/10645251_10150004552801937_4553731092814901385_n.jpg?_nc_cat=1&efg=eyJpIjoiYiJ9&_nc_ht=scontent.fclj2-1.fna&oh=12e325c3d35a470bb016a3a472aeb057&oe=5D914140',
+    'https://scontent.fclj2-1.fna.fbcdn.net/v/t1.0-1/cp0/e15/q65/c15.0.50.50a/p50x50/1379841_10150004552801901_469209496895221757_n.jpg?_nc_cat=1&efg=eyJpIjoiYiJ9&_nc_ht=scontent.fclj2-1.fna&oh=7ef0d577ed46ba37e83fe945204953b6&oe=5D97BE80'
+        ]
---
+++ face_crawler/src/setup_crawler.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orm.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+
+    execute_from_command_line([sys.argv[0], "makemigrations", "models"])
+    execute_from_command_line([sys.argv[0], "migrate"])
+    req_path = os.path.join('..', 'requirements.txt')
+    os.system(f"pip install -r {req_path}")
